de28c7a021177539572cad75884d88dc2f08a08b
deficto <deficto@protonmail.com>
2023-10-28 10:05:51 -0400

    Rename files

---
+++ src/NFTContract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "solmate/tokens/ERC721.sol";
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
+
+contract NFTContract {
+    uint256 public number;
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
